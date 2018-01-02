9439a575860ed82c9423fc6c73155b1004b0e8b9
dylee <leeduyoung20@naver.com>
2018-01-02 22:53:12 +0900

    * ng-scrollreveal 플러그인 추

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {NgsRevealModule} from 'ng-scrollreveal';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,7 +13,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgsRevealModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
