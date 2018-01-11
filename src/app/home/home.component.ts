f6f234f5cab0b78808db5fffafa3e1ee4b91dc3d
dylee <leeduyoung20@naver.com>
2018-01-11 23:48:13 +0900

    slick slider 작업중

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'slick-carousel';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +14,22 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
+    $(".slide-container").slick({
+      autoplay : false,
+      dots: true,
+      speed : 300 /* 이미지가 슬라이딩시 걸리는 시간 */,
+      infinite: true,
+      autoplaySpeed: 3000 /* 이미지가 다른 이미지로 넘어 갈때의 텀 */,
+      arrows: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      fade: false
+    });
+
+  }
+
+  ngAfterViewInit() {
+
   }
 
 }
