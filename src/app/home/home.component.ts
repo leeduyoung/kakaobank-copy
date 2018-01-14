1e7ee8b18c3b77f57ff054ac7fa6fc11a820655f
dylee <leeduyoung20@naver.com>
2018-01-14 23:53:12 +0900

    content-5 작업중 - slick css 수정 - encapsulation 설정 none으로 변경. slick에 css가 적용되지 않는 문제가 있음.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import * as $ from 'jquery';
 import 'slick-carousel';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
 
@@ -16,16 +19,17 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     $(".slide-container").slick({
       autoplay : false,
-      dots: true,
+      dots: false,
       speed : 300 /* 이미지가 슬라이딩시 걸리는 시간 */,
       infinite: true,
       autoplaySpeed: 3000 /* 이미지가 다른 이미지로 넘어 갈때의 텀 */,
       arrows: true,
       slidesToShow: 3,
       slidesToScroll: 1,
-      fade: false
+      fade: false,
+      centerMode: true,
+      centerPadding: '40px'
     });
-
   }
 
   ngAfterViewInit() {
